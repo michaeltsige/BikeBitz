a08f981a21666ef92c4f65b674e79df3051cf116
michaeltsige <michaeltsige11@gmail.com>
2023-04-18 21:04:48 +0530

    changes in bikecard and modal

--- src/components/bike-card/bike.card.jsx
+++ src/components/bike-card/bike.card.jsx
@@ -1,28 +1,38 @@
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
-import "./bike-card.css";
-import { useState } from 'react';
-
 import { RentModal } from '../rent-modal/rent-modal';
-import { Modal } from 'bootstrap';
+
+import './bike-card.css';
 
 export const BikeCard = (props) => {
-  
+  const [showRentModal, setShowRentModal] = useState(false);
 
+  const handleRentClick = () => {
+    setShowRentModal(true);
+  };
+
+  const handleRentModalClose = () => {
+    setShowRentModal(false);
+  };
 
   return (
     <>
-    <Card style={{ width: '18rem', height: '20rem', backgroundColor:'white'}}>
-      <Card.Img variant="top" style={{borderRadius: '15px'}}  src={props.image} />
-      <Card.Body>
-        <Card.Title ><p className="t1">{props.name}</p></Card.Title>
-        <Card.Text>
-          <p className="t2">{props.rate} wei</p>
-        </Card.Text>
-        <Button variant="primary" className="rent-button" >Rent</Button>
-      </Card.Body>
-    </Card>
+      <Card style={{ width: '18rem', height: '20rem', backgroundColor: 'white' }}>
+        <Card.Img variant="top" style={{ borderRadius: '15px' }} src={props.image} />
+        <Card.Body>
+          <Card.Title>
+            <p className="t1">{props.name}</p>
+          </Card.Title>
+          <Card.Text>
+            <p className="t2">{props.rate} wei</p>
+          </Card.Text>
+          <Button variant="primary" className="rent-button" onClick={handleRentClick}>
+            Rent
+          </Button>
+        </Card.Body>
+      </Card>
+      {showRentModal? (<RentModal show={showRentModal} handleClose={handleRentModalClose} />):(<></>)}
     </>
   );
-}
+};
--- src/components/rent-modal/rent-modal.jsx
+++ src/components/rent-modal/rent-modal.jsx
@@ -1,26 +1,45 @@
-
+import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
-import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-export const RentModal = ( { closeModal }) => {
+export const RentModal = ({ closeModal }) => {
+  const [rentHours, setRentHours] = useState('');
+
+  const handleRent = () => {
+    // Handle rent calculation and submission
+  };
+
+  const handleChange = (e) => {
+    setRentHours(e.target.value);
+  };
 
   return (
-    <div className='modalBackground'>
-      <div className='modalContainer'>
-        <div className="titleCloseBtn">
-          <button className="modalButton" onclick={() => closeModal(false)}> X </button>
-        </div>
-        <div className='modalTitle'>
-          <h1>Are you sure you want to continue</h1>
-        </div>
-        <div className='modalBody'>
-          <p>Modal Body</p>
-        </div>
-        <div className='modalFooter'>
-          <button className="modalButton">Continue</button>
-          <button className="modalButton" onclick={() => closeModal(false)}>Cancel</button>
-        </div>
-      </div>
-    </div>
+    <Modal show onHide={() => closeModal(false)}>
+      <Modal.Header closeButton>
+        <Modal.Title>Rent Bike</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group>
+            <Form.Label>Rent Hours</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter rent hours"
+              value={rentHours}
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={() => closeModal(false)}>
+          Cancel
+        </Button>
+        <Button variant="primary" onClick={handleRent}>
+          Rent
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
-}
+};
